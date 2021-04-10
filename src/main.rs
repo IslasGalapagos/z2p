c8266d9c52e4a9e856e25be43f55b7226b8e0bd5
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2021-04-10 22:43:22 +0300

    made base structure; set connections; added script to run docker; set configuration; added some integration tests; etc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use z2p::startup::run;
+use z2p::configuration::get_configuration;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
+
+    run(listener)?.await
 }
