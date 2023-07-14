8c9c2ac68bef63202693f35330caebb711517b32
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2023-07-14 21:32:11 +0400

    subscriptions route; project config; db connection and migrations

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,18 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+
+use z2p::configuration::get_configuration;
+use z2p::startup::run;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_works() {
@@ -16,22 +30,11 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = z2p::run(listener).expect("Failed to bind address");
-
-    let _ = tokio::spawn(server);
-
-    format!("http://127.0.0.1:{}", port)
-}
-
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    // Arrange
     let app_address = spawn_app();
     let client = reqwest::Client::new();
-    // Act
+
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
@@ -40,8 +43,22 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .send()
         .await
         .expect("Failed to execute request.");
-    // Assert
+
     assert_eq!(200, response.status().as_u16());
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
