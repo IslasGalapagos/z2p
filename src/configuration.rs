5c5b5c5d454db5bf83a2d86f62a93414750f0464
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2023-07-30 21:54:08 +0400

    fixed setting log options

--- src/configuration.rs
+++ src/configuration.rs
@@ -38,15 +38,15 @@ impl DatabaseSettings {
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
-            .password(&self.password.expose_secret())
+            .password(self.password.expose_secret())
             .port(self.port)
             .ssl_mode(ssl_mode)
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        let mut options = self.without_db().database(&self.database_name);
-        options.log_statements(tracing::log::LevelFilter::Trace);
-        options
+        self.without_db()
+            .database(&self.database_name)
+            .log_statements(tracing::log::LevelFilter::Trace)
     }
 }
 
